6928661aa73f5116bee5d0c16df78b98658607e8
AshSeitz <ash.seitz94@gmail.com>
2018-10-13 21:49:49 -0400

    git commit -m "js psuedo code and variable start"

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+//creat variables for each question
+var questions = [
+    {q:"Who played the first doctor in the reboot 2005?", a:"Christopher Eccelston"},
+    {q:"How many tv spin-offs does Doctor Who have?", a:"6"},
+    {q:"Who were the main enemies in the 50th Anniversary?", a:"zygons"},
+    {q:"Cassandra is the name the female version of the Master takes on." , a:"f"},
+    {q:"Who was the main character in the episode 'Blink'?", a:"Sally Sparrow"},
+    {q:"What companion has her own spin-off and appears in both the reboot and the classic series", a:"Sarah Jane"},
+    {q:"There was a great war between Time Lords and Vampires", a:"t"},
+    {q:"Who suceeded Harriot Jones as the Prime Minister", a:"Harold Saxon"},
+    {q:"Who are the Doctor's archenemies?", a:"Daleks"},
+    {q:"Weeping Angels first appear in the episode'The Angels Take Manhatten'.", a:"f"},
+]
+
+var userInput
+var correct = 0;
+var incorrect = 0;
+var unanswered = 0;
+var questionIndex = 0;
+//if correct answer is chosen add 1 to correct answers
+    if (userInput === questions[questionIndex].a){
+        console.log(win)
+    }
+//if wrong answer is chosen add 1 to incorrect answers
+
+//if no answer is chosen and timer runs out add 1 to missed answers
+
+//create timer
+
+//display timer
+
